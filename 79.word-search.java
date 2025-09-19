a54df3bbf4f88757f236ba19f0deebe624acd2fe
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2025-09-19 09:50:38 +0530

    Add solution for Word Search problem

---
+++ 79.word-search.java
@@ -0,0 +1,116 @@
+/*
+ * @lc app=leetcode id=79 lang=java
+ *
+ * [79] Word Search
+ *
+ * https://leetcode.com/problems/word-search/description/
+ *
+ * algorithms
+ * Medium (45.90%)
+ * Likes:    17150
+ * Dislikes: 730
+ * Total Accepted:    2.2M
+ * Total Submissions: 4.9M
+ * Testcase Example:  '[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]\n"ABCCED"'
+ *
+ * Given an m x n grid of characters board and a string word, return true if
+ * word exists in the grid.
+ * 
+ * The word can be constructed from letters of sequentially adjacent cells,
+ * where adjacent cells are horizontally or vertically neighboring. The same
+ * letter cell may not be used more than once.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word
+ * = "ABCCED"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word
+ * = "SEE"
+ * Output: true
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word
+ * = "ABCB"
+ * Output: false
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * m == board.length
+ * n = board[i].length
+ * 1 <= m, n <= 6
+ * 1 <= word.length <= 15
+ * board and word consists of only lowercase and uppercase English letters.
+ * 
+ * 
+ * 
+ * Follow up: Could you use search pruning to make your solution faster with a
+ * larger board?
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+
+        // Prune by character frequency
+        int[] charCount = new int[58]; // 'A' to 'z'
+        for (char[] row : board) {
+            for (char c : row) {
+                charCount[c - 'A']++;
+            }
+        }
+
+        for (char c : word.toCharArray()) {
+            if (--charCount[c - 'A'] < 0) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int i, int j, int k) {
+        if (k == word.length()) {
+            return true;
+        }
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(k)) {
+            return false;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = '#';  // Mark as visited
+
+        boolean found = dfs(board, word, i + 1, j, k + 1) ||
+                        dfs(board, word, i - 1, j, k + 1) ||
+                        dfs(board, word, i, j + 1, k + 1) ||
+                        dfs(board, word, i, j - 1, k + 1);
+
+        board[i][j] = temp;  // Restore original value
+        return found;
+    }
+}
+// @lc code=end
+
