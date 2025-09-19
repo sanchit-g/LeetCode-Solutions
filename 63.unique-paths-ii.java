abc811f3970345e908875f59d64166795205ea09
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2025-09-19 09:33:59 +0530

    Add solution for Unique Paths II problem

---
+++ 63.unique-paths-ii.java
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode id=63 lang=java
+ *
+ * [63] Unique Paths II
+ *
+ * https://leetcode.com/problems/unique-paths-ii/description/
+ *
+ * algorithms
+ * Medium (43.56%)
+ * Likes:    9417
+ * Dislikes: 550
+ * Total Accepted:    1.3M
+ * Total Submissions: 3M
+ * Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
+ *
+ * You are given an m x n integer array grid. There is a robot initially
+ * located at the top-left corner (i.e., grid[0][0]). The robot tries to move
+ * to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only
+ * move either down or right at any point in time.
+ * 
+ * An obstacle and space are marked as 1 or 0 respectively in grid. A path that
+ * the robot takes cannot include any square that is an obstacle.
+ * 
+ * Return the number of possible unique paths that the robot can take to reach
+ * the bottom-right corner.
+ * 
+ * The testcases are generated so that the answer will be less than or equal to
+ * 2 * 10^9.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+ * Output: 2
+ * Explanation: There is one obstacle in the middle of the 3x3 grid above.
+ * There are two ways to reach the bottom-right corner:
+ * 1. Right -> Right -> Down -> Down
+ * 2. Down -> Down -> Right -> Right
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: obstacleGrid = [[0,1],[0,0]]
+ * Output: 1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * m == obstacleGrid.length
+ * n == obstacleGrid[i].length
+ * 1 <= m, n <= 100
+ * obstacleGrid[i][j] is 0 or 1.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+
+        Integer[][] memo = new Integer[m][n];
+
+        // Recursive DFS
+        return dfs(obstacleGrid, 0, 0, m, n, memo);
+    }
+
+    private int dfs(int[][] grid, int x, int y, int m, int n, Integer[][] memo) {
+        if (x >= m || y >= n || grid[x][y] == 1) {
+            return 0;
+        }
+        
+        if (x == m - 1 && y == n - 1) {
+            return 1;
+        }
+
+        if (memo[x][y] != null) {
+            return memo[x][y];
+        }
+
+        memo[x][y] = dfs(grid, x + 1, y, m, n, memo) + dfs(grid, x, y + 1, m, n, memo);
+        return memo[x][y];
+    }
+}
+// @lc code=end
+
