3412dab3e0a0ddef1a708849559bbaa153f58d49
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2025-09-20 11:19:08 +0530

    Add solution for Combination Sum IV problem

---
+++ 377.combination-sum-iv.java
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode id=377 lang=java
+ *
+ * [377] Combination Sum IV
+ *
+ * https://leetcode.com/problems/combination-sum-iv/description/
+ *
+ * algorithms
+ * Medium (54.75%)
+ * Likes:    7665
+ * Dislikes: 693
+ * Total Accepted:    569.8K
+ * Total Submissions: 1M
+ * Testcase Example:  '[1,2,3]\n4'
+ *
+ * Given an array of distinct integers nums and a target integer target, return
+ * the number of possible combinations that add up to target.
+ * 
+ * The test cases are generated so that the answer can fit in a 32-bit
+ * integer.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,2,3], target = 4
+ * Output: 7
+ * Explanation:
+ * The possible combination ways are:
+ * (1, 1, 1, 1)
+ * (1, 1, 2)
+ * (1, 2, 1)
+ * (1, 3)
+ * (2, 1, 1)
+ * (2, 2)
+ * (3, 1)
+ * Note that different sequences are counted as different combinations.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [9], target = 3
+ * Output: 0
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 200
+ * 1 <= nums[i] <= 1000
+ * All the elements of nums are unique.
+ * 1 <= target <= 1000
+ * 
+ * 
+ * 
+ * Follow up: What if negative numbers are allowed in the given array? How does
+ * it change the problem? What limitation we need to add to the question to
+ * allow negative numbers?
+ * 
+ */
+
+// @lc code=start
+import java.util.*;
+
+class Solution {
+    private int[] dp;
+
+    public int combinationSum4(int[] nums, int target) {
+        dp = new int[target + 1];
+        Arrays.fill(dp, -1);
+        return combinationSum4Helper(nums, target);
+    }
+
+    private int combinationSum4Helper(int[] nums, int target) {
+        if (target == 0) {
+            return 1;
+        }
+        if (target < 0) {
+            return 0;
+        }
+        if (dp[target] != -1) {
+            return dp[target];
+        }
+
+        int ways = 0;
+        for (int num : nums) {
+            ways += combinationSum4Helper(nums, target - num);
+        }
+        dp[target] = ways;
+        return ways;
+    }
+}
+// @lc code=end
+
