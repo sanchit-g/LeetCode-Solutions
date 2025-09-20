f7f8044f9c93003c21af4c83a36657cb81825f82
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2025-09-20 09:47:18 +0530

    Add solution for Permutations II problem

---
+++ 47.permutations-ii.java
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=47 lang=java
+ *
+ * [47] Permutations II
+ *
+ * https://leetcode.com/problems/permutations-ii/description/
+ *
+ * algorithms
+ * Medium (62.16%)
+ * Likes:    8954
+ * Dislikes: 158
+ * Total Accepted:    1.2M
+ * Total Submissions: 1.9M
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given a collection of numbers, nums, that might contain duplicates, return
+ * all possible unique permutations in any order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,1,2]
+ * Output:
+ * [[1,1,2],
+ * ⁠[1,2,1],
+ * ⁠[2,1,1]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1,2,3]
+ * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 8
+ * -10 <= nums[i] <= 10
+ * 
+ * 
+ */
+
+// @lc code=start
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        Arrays.sort(nums); // Sort to handle duplicates
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, used, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int[] nums, boolean[] used, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+
+            used[i] = true;
+            current.add(nums[i]);
+            backtrack(nums, used, current, result);
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+}
+// @lc code=end
+
