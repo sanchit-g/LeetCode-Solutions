44e50a08fa3fcbb8f7a071c90da3ec6a3705822c
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2025-09-20 09:30:49 +0530

    Add solution for Permutations problem

---
+++ 46.permutations.java
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=46 lang=java
+ *
+ * [46] Permutations
+ *
+ * https://leetcode.com/problems/permutations/description/
+ *
+ * algorithms
+ * Medium (81.09%)
+ * Likes:    20328
+ * Dislikes: 368
+ * Total Accepted:    2.8M
+ * Total Submissions: 3.4M
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given an array nums of distinct integers, return all the possible
+ * permutations. You can return the answer in any order.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [1,2,3]
+ * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * Example 2:
+ * Input: nums = [0,1]
+ * Output: [[0,1],[1,0]]
+ * Example 3:
+ * Input: nums = [1]
+ * Output: [[1]]
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * All the integers of nums are unique.
+ * 
+ * 
+ */
+
+// @lc code=start
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, used, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int[] nums, boolean[] used, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+
+            used[i] = true;
+            current.add(nums[i]);
+            backtrack(nums, used, current, result);
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+}
+// @lc code=end
+
