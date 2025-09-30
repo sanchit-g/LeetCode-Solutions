67558d022d2671ce64d33b50afe0d3a5631b6aac
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2025-09-30 23:30:44 +0530

    Add DP solution for longest increasing subsequence

--- 300.longest-increasing-subsequence.java
+++ 300.longest-increasing-subsequence.java
@@ -58,12 +58,31 @@
 import java.util.*;
 
 class Solution {
-    public int lengthOfLIS(int[] nums) {
+    public int lengthOfLIS2(int[] nums) {
         int[][] memo = new int[nums.length][nums.length + 1];
         for (int i = 0; i < nums.length; i++) {
             Arrays.fill(memo[i], -1);
         }
         return recursiveLIS(nums, 0, -1, memo);
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) return 0;
+        
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1); // Each element is an increasing subsequence of length 1
+
+        int maxLength = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        
+        return maxLength;
     }
 
     private int recursiveLIS(int[] nums, int currentIndex, int previousIndex, int[][] memo) {
