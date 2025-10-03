4b854d8a70f6501ed8cb2d3c4cb31eacbe0f00e3
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2025-10-04 02:35:43 +0530

    Add solution for Insert Interval problem

---
+++ 57.insert-interval.java
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode id=57 lang=java
+ *
+ * [57] Insert Interval
+ *
+ * https://leetcode.com/problems/insert-interval/description/
+ *
+ * algorithms
+ * Medium (44.06%)
+ * Likes:    11400
+ * Dislikes: 900
+ * Total Accepted:    1.6M
+ * Total Submissions: 3.6M
+ * Testcase Example:  '[[1,3],[6,9]]\n[2,5]'
+ *
+ * You are given an array of non-overlapping intervals intervals where
+ * intervals[i] = [starti, endi] represent the start and the end of the i^th
+ * interval and intervals is sorted in ascending order by starti. You are also
+ * given an interval newInterval = [start, end] that represents the start and
+ * end of another interval.
+ * 
+ * Insert newInterval into intervals such that intervals is still sorted in
+ * ascending order by starti and intervals still does not have any overlapping
+ * intervals (merge overlapping intervals if necessary).
+ * 
+ * Return intervals after the insertion.
+ * 
+ * Note that you don't need to modify intervals in-place. You can make a new
+ * array and return it.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+ * Output: [[1,5],[6,9]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+ * Output: [[1,2],[3,10],[12,16]]
+ * Explanation: Because the new interval [4,8] overlaps with
+ * [3,5],[6,7],[8,10].
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= intervals.length <= 10^4
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 10^5
+ * intervals is sorted by starti in ascending order.
+ * newInterval.length == 2
+ * 0 <= start <= end <= 10^5
+ * 
+ * 
+ */
+
+// @lc code=start
+import java.util.*;
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        
+        // if the list of intervals is empty, just return the new interval
+        if (intervals.length == 0) {
+            return new int[][] {newInterval};
+        }
+
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+        int n = intervals.length;
+
+        // add all intervals ending before newInterval starts
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        // merge all overlapping intervals to one considering newInterval
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+
+        result.add(newInterval); // add the merged interval
+
+        // add all the rest of the intervals
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        return result.toArray(int[][]::new);
+    }
+}
+// @lc code=end
+
