a953045d0ef634b1b56724e644e795f13cd5a6f0
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2025-10-03 19:47:55 +0530

    Add solution for Search a 2D Matrix II problem

---
+++ 240.search-a-2-d-matrix-ii.java
@@ -0,0 +1,112 @@
+/*
+ * @lc app=leetcode id=240 lang=java
+ *
+ * [240] Search a 2D Matrix II
+ *
+ * https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+ *
+ * algorithms
+ * Medium (56.04%)
+ * Likes:    12793
+ * Dislikes: 223
+ * Total Accepted:    1.2M
+ * Total Submissions: 2.2M
+ * Testcase Example:  '[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]\n' +
+  '5'
+ *
+ * Write an efficient algorithm that searches for a value target in an m x n
+ * integer matrix matrix. This matrix has the following properties:
+ * 
+ * 
+ * Integers in each row are sorted in ascending from left to right.
+ * Integers in each column are sorted in ascending from top to bottom.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: matrix =
+ * [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],
+ * target = 5
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: matrix =
+ * [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],
+ * target = 20
+ * Output: false
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= n, m <= 300
+ * -10^9 <= matrix[i][j] <= 10^9
+ * All the integers in each row are sorted in ascending order.
+ * All the integers in each column are sorted in ascending order.
+ * -10^9 <= target <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    private int rows, cols;
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        rows = matrix.length;
+        cols = matrix[0].length;
+        
+        int row = 0, col = cols - 1; // Start from the top-right corner
+        
+        while (row < rows && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true; // Target found
+            } else if (matrix[row][col] > target) {
+                col--; // Move left
+            } else {
+                row++; // Move down
+            }
+        }
+        return false;
+    }
+
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        rows = matrix.length;
+        cols = matrix[0].length;
+        
+        // Perform binary search on each row
+        for (int i = 0; i < rows; i++) {
+            if (binarySearch(matrix, target, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean binarySearch(int[][] matrix, int target, int row) {
+        int left = 0;
+        int right = cols - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (matrix[row][mid] == target) {
+                return true;
+            } else if (matrix[row][mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
