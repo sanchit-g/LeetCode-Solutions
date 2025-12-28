2a48e1e46066c88c1db79bc030af8933d6c565ac
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2025-12-28 21:22:41 +0530

    Add solution for Sliding Window Maximum problem

---
+++ 239.sliding-window-maximum.java
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=239 lang=java
+ *
+ * [239] Sliding Window Maximum
+ *
+ * https://leetcode.com/problems/sliding-window-maximum/description/
+ *
+ * algorithms
+ * Hard (48.16%)
+ * Likes:    20004
+ * Dislikes: 810
+ * Total Accepted:    1.5M
+ * Total Submissions: 3.2M
+ * Testcase Example:  '[1,3,-1,-3,5,3,6,7]\n3'
+ *
+ * You are given an array of integers nums, there is a sliding window of size k
+ * which is moving from the very left of the array to the very right. You can
+ * only see the k numbers in the window. Each time the sliding window moves
+ * right by one position.
+ * 
+ * Return the max sliding window.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+ * Output: [3,3,5,5,6,7]
+ * Explanation: 
+ * Window position                Max
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * ⁠1 [3  -1  -3] 5  3  6  7       3
+ * ⁠1  3 [-1  -3  5] 3  6  7       5
+ * ⁠1  3  -1 [-3  5  3] 6  7       5
+ * ⁠1  3  -1  -3 [5  3  6] 7       6
+ * ⁠1  3  -1  -3  5 [3  6  7]      7
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1], k = 1
+ * Output: [1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
+ * 1 <= k <= nums.length
+ * 
+ * 
+ */
+
+// @lc code=start
+import java.util.*;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] res = new int[nums.length - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
+                deque.pollLast();
+            }
+            
+            deque.offerLast(i);
+
+            if (i >= k - 1) {
+                res[i - k + 1] = nums[deque.peekFirst()];
+            }
+
+            if (i - deque.peekFirst() >= k - 1) {
+                deque.pollFirst();
+            }
+        }
+
+        return res;
+    }
+}
+// @lc code=end
