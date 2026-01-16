51ffe8deb6b23e305f15241ed344e3a77780b6cb
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2026-01-16 20:29:28 +0530

    Add solution for Container With Most Water problem

---
+++ 11.container-with-most-water.java
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode id=11 lang=java
+ *
+ * [11] Container With Most Water
+ *
+ * https://leetcode.com/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (59.07%)
+ * Likes:    33452
+ * Dislikes: 2150
+ * Total Accepted:    4.9M
+ * Total Submissions: 8.3M
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * You are given an integer array height of length n. There are n vertical
+ * lines drawn such that the two endpoints of the i^th line are (i, 0) and (i,
+ * height[i]).
+ * 
+ * Find two lines that together with the x-axis form a container, such that the
+ * container contains the most water.
+ * 
+ * Return the maximum amount of water a container can store.
+ * 
+ * Notice that you may not slant the container.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: height = [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * Explanation: The above vertical lines are represented by array
+ * [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the
+ * container can contain is 49.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: height = [1,1]
+ * Output: 1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * n == height.length
+ * 2 <= n <= 10^5
+ * 0 <= height[i] <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int left = 0;
+        int right = n - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+            
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
+// @lc code=end
