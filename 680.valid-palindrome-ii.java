d93c2e32df410bf51fed22fcee0d370c7e0ab389
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2026-01-16 20:50:21 +0530

    Add solution for Valid Palindrome II problem

---
+++ 680.valid-palindrome-ii.java
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=680 lang=java
+ *
+ * [680] Valid Palindrome II
+ *
+ * https://leetcode.com/problems/valid-palindrome-ii/description/
+ *
+ * algorithms
+ * Easy (43.71%)
+ * Likes:    8871
+ * Dislikes: 507
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.5M
+ * Testcase Example:  '"aba"'
+ *
+ * Given a string s, return true if the s can be palindrome after deleting at
+ * most one character from it.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "aba"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "abca"
+ * Output: true
+ * Explanation: You could delete the character 'c'.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: s = "abc"
+ * Output: false
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 10^5
+ * s consists of lowercase English letters.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+// @lc code=end
